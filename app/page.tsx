b271d8166048316288928071eba82753cd1aef45
Abdelaziz Ebrahim <abdelazizebrahim5980@gmail.com>
2025-01-07 22:55:49 +0300

    fixed icon properties

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,83 @@
 "use client";
 
 import LandingSections from "@/components/LandingSections";
+import { useEffect, useRef, useState } from "react";
+
+import { motion, AnimationPresence, AnimatePresence} from 'framer-motion';
+import { useChat } from "@ai-sdk/react";
+
+import ReactMarkdown from "react-markdown";
+import remarkGmf from "remark-gfm";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card, 
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import { 
+  X,
+  MessageCircle,
+  Send,
+  Loader2,
+  ArrowDownCircleIcon
+} from "lucide-react";
 
 export default function Chat() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [showChatIcon, setShowChatIcon] = useState(false);
+  const chatIconRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY > 200) {
+        setShowChatIcon(true);
+      } else {
+        setShowChatIcon(false);
+        setIsChatOpen(false);
+      }
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
       <LandingSections />
+      <AnimatePresence>
+        { showChatIcon && (
+          <motion.div
+            initial = {{ opacity: 0, y: 100}}
+            animate = {{ opacity: 1, y: 0}}
+            exit={{ opacity: 0, y:100}}
+            transition={{ duration: 0.2}}
+            className="fixed bottom-4 right-4 z-50">
+              <Button
+                ref={chatIconRef}
+                onClick={toggleChat}
+                size={"icon"}
+                className="rounded-full size-14 p-2 shadow-lg">
+                  {!isChatOpen ? (<MessageCircle className="size-12"/>) : (<ArrowDownCircleIcon/>)}
+                </Button>
+          </motion.div>
+        )
+
+        } 
+      </AnimatePresence>
     </div>
   );
 }
